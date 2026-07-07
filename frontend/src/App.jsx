b94384188121db96c8aac49200fa236f5faa732a
Danuj-kethaka <senanayakakethaka@gmail.com>
2026-07-07 19:53:35 +0530

    home page and app jsx updated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import Login from "./pages/Auth/LoginPage.jsx"
+import { Routes,Route } from 'react-router-dom'
+import Register from "./pages/Auth/RegisterPage.jsx"
+import RestaurantHomepage from "./pages/HomePage.jsx"
+import UserSideBar from "./components/UserSideBar.jsx"
+import AdminSideBar from "./components/AdminSideBar.jsx"
+import ContactUs from "./pages/ContactUs";
+import Gallery from "./pages/GalleryPage.jsx"
+import Account from "./pages/Auth/UserPage.jsx"
+import { useUserStore } from "./store/Auth/User.js";
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+
+  return (
+    <>
+     <Routes>
+      <Route path="/" element={<RestaurantHomepage/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/admindashboard" element={<AdminSideBar/>} />
+      <Route path="/useraccount" element={<UserSideBar/>} />
+      <Route path="/contact" element={<ContactUs />} />
+      <Route path="/gallery" element={<Gallery />} />x
+      
+
+      <Route path="/useraccount" element={<UserSideBar />}>
+       <Route index element={<Account />} />
+      </Route>
+        
+     </Routes>
+    </>
+  )
+}
+
+export default App
+
